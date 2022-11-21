30c1c65ccd2e7ad0e3a3d546e8869739998d5c6c
calissonn <calissonferreira59@gmail.com>
2022-11-21 20:56:23 -0300

    Criado paginas

---
+++ Android/Aula05-25-08-2022/app/src/main/java/br/edu/alfaumuarama/aula05_25_08_2022/Config.java
@@ -0,0 +1,6 @@
+package br.edu.alfaumuarama.aula05_25_08_2022;
+
+public class Config {
+    public static String urlApi = "https://rickandmortyapi.com/api/character?page=1";
+    public static String linkImagem = "https://rickandmortyapi.com/api/character/avatar/";
+}
